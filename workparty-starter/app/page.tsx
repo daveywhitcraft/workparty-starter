d3b7d179d7f9e4535f76d1cd91a142fac86f9089
daveywhitcraft <106935961+daveywhitcraft@users.noreply.github.com>
2025-09-17 15:51:28 +0200

    Update page.tsx

--- workparty-starter/app/page.tsx
+++ workparty-starter/app/page.tsx
@@ -2,12 +2,7 @@ export default function Home() {
   return (
     <section>
       <h1 className="title">WORK.PARTY</h1>
-      <p>Monthly video evenings anti-curated by WORK.PARTY</p>
-      <ul>
-        <li>Submit to our city-specific monthly screenings here.</li>
-        <li>Current edition streams here during the event.</li>
-        <li>Archive lists artists and works from previous editions.</li>
-      </ul>
+   
     </section>
   );
 }
