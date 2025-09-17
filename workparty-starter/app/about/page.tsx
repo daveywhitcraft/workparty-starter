0cdc2025ddb350d5440d8cac1fdcbc87559eb484
daveywhitcraft <106935961+daveywhitcraft@users.noreply.github.com>
2025-09-17 19:02:03 +0200

    Update page.tsx

--- workparty-starter/app/about/page.tsx
+++ workparty-starter/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <section>
-      <h1 className="title">About</h1>
+      
       <p>WORK.PARTY is a collaborative screening platform by Ismael Peltreau and Davey Whitcraft. City-based calls. Local events. Ongoing index.</p>
       <p>Contact: workwork.party@gmail.com</p>
     </section>
