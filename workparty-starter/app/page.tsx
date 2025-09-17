ba91a66f3355ad4ce7b2661176f77f1c60131ede
daveywhitcraft <106935961+daveywhitcraft@users.noreply.github.com>
2025-09-17 18:30:14 +0200

    Update page.tsx

--- workparty-starter/app/page.tsx
+++ workparty-starter/app/page.tsx
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <section
-      className="min-h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('/bg.jpg')" }}
-    >
-      <h1 className="text-white text-4xl text-center pt-20">Hello World</h1>
-    </section>
-  );
+  return <section></section>;
 }
