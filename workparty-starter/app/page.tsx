28e974f659630013c92a6a544e3bb3180394a2f0
daveywhitcraft <106935961+daveywhitcraft@users.noreply.github.com>
2025-09-17 17:43:47 +0200

    Update page.tsx

--- workparty-starter/app/page.tsx
+++ workparty-starter/app/page.tsx
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
-    <section>
-   className="min-h-screen bg-cover bg-center"
+    <section
+      className="min-h-screen bg-cover bg-center"
       style={{ backgroundImage: "url('/bg.jpg')" }}
     >
+      {/* optional overlay + content */}
+      <div className="min-h-screen bg-black/50 flex items-center justify-center text-white text-center px-4">
+        <h1 className="title">WORK.PARTY</h1>
+      </div>
     </section>
   );
 }
