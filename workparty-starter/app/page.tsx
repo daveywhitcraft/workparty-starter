73f3149ffd72f4c7a06f32d50567a9207ac3f1b1
daveywhitcraft <106935961+daveywhitcraft@users.noreply.github.com>
2025-09-20 09:42:06 +0200

    Update page.tsx

--- workparty-starter/app/page.tsx
+++ workparty-starter/app/page.tsx
@@ -1,21 +1,21 @@
 // app/page.tsx
 export default function Home() {
   return (
-    <section className="relative flex items-center justify-center min-h-[calc(100vh-88px)] px-6">
-      <div className="text-center space-y-6 bg-black/40 px-6 py-8 rounded-xl">
-        <h1 className="text-4xl md:text-6xl font-semibold tracking-tight">
+    <section className="flex items-center justify-center min-h-[calc(100vh-88px)] px-6">
+      <div className="text-center space-y-12">
+        <h1 className="text-6xl md:text-9xl font-bold tracking-tight">
           NEXT WORK.PARTY
         </h1>
 
-        <p className="text-xl md:text-3xl leading-snug">
-          28/10/2025&nbsp;&nbsp;20:00-? <br className="md:hidden" />
+        <p className="text-3xl md:text-6xl leading-snug">
+          28/10/2025&nbsp;&nbsp;20:00-? <br />
           Schererstraße 10, 13347 Wedding Berlin
         </p>
 
         <div>
           <a
             href="/submit"
-            className="inline-block text-lg md:text-2xl underline underline-offset-4 hover:opacity-90"
+            className="inline-block text-3xl md:text-6xl underline underline-offset-8 hover:opacity-80"
           >
             SUBMIT NOW
           </a>
