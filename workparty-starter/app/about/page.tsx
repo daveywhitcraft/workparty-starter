8caf0bb56e832656a80dc72bedb2ff087a29e6fb
daveywhitcraft <106935961+daveywhitcraft@users.noreply.github.com>
2025-09-24 09:56:37 +0200

    Update page.tsx

--- workparty-starter/app/about/page.tsx
+++ workparty-starter/app/about/page.tsx
@@ -3,8 +3,8 @@ export default function AboutPage() {
     <section style={{ padding: "24px" }}>
       <div style={{ maxWidth: "70ch" }}>
         <p style={{ margin: 0, lineHeight: 1.5 }}>
-          WORK.PARTY is an itinerant project of collective screening events from Ismael Peltreau and Davey Whitcraft.<br />
-          City-based open calls/events.<br />
+          WORK.PARTY is an itinerant project of collective screening events<br />
+          Check our <a href="https://www.instagram.com/workparty.video/">Instagram</a> for current events.<br />
         
 
         </p>
