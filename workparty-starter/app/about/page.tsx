cfe24e74a3737390384b0800b8f67024c6fb0892
daveywhitcraft <106935961+daveywhitcraft@users.noreply.github.com>
2025-09-26 13:26:29 +0200

    Update page.tsx

--- workparty-starter/app/about/page.tsx
+++ workparty-starter/app/about/page.tsx
@@ -4,6 +4,7 @@ export default function AboutPage() {
       <div style={{ maxWidth: "70ch" }}>
         <p style={{ margin: 0, lineHeight: 1.5 }}>
           WORK.PARTY is an itinerant project of collective screening events<br />
+           We gather and screen SHORT (<10 mins) videos from LOCAL artists and all gather to watch and chat and have fun. Please consider submitting if you can join<br />
           Check our <a href="https://www.instagram.com/workparty.video/">Instagram</a> for current events.<br />
         
 
