40174eb04fe8797969b988c880a09b095b2d0519
daveywhitcraft <106935961+daveywhitcraft@users.noreply.github.com>
2025-11-01 15:20:38 +0100

    Update page.tsx

--- workparty-starter/app/page.tsx
+++ workparty-starter/app/page.tsx
@@ -38,7 +38,6 @@ export default function Page() {
           >
             16/11/2025&nbsp;&nbsp;19:00-21:00 <br />
             Schererstraße 10, 13347 Berlin <br />
-            Submissions Open!
           </div>
  
           <a
